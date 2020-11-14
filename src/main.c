ba805ae171a83d5b970969d64b4efebb5cefef71
Florian CLOAREC <39800745+Flo3171@users.noreply.github.com>
2020-11-14 19:32:59 +0100

    ajout cahier des charge (à addapter au projet)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
+/**
+ * @file main.c
+ * @author Florian CLOAREC
+ * @brief fichier contenant la fonction main
+ * @version 0.1
+ * @date 2020-11-14
+ *  
+ */
 #include "stdlib.h"
 #include "stdio.h"
 
+/**
+ * @brief fonction main qui appelle toutes les autres fonction du programme
+ * 
+ * @param argc : nombre d'arguement
+ * @param argv : tableu contenant les arguement
+ * @return int : vaut EXIT_SUCCESS (0) si tout c'est bien passé et EXIT_FAILLURE (1) si il y à eut un problème lors de l'éxécution
+ */
 int main(int argc, char *argv[])
 {
     printf("Hello world !");
-
+    
     return EXIT_SUCCESS;
 }
---
+++ src/struct.h
@@ -0,0 +1,9 @@
+/**
+ * @file struct.h
+ * @author Florian CLOAREC
+ * @brief fichier contenant toute les structure de données utilisée dans le projets
+ * @version 0.1
+ * @date 2020-11-14
+ * 
+ */
+
